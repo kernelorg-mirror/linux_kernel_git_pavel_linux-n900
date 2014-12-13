7e1c13562c60c28e3b6a248dac95072061b8748d
Pavel <pavel@ucw.cz>
2014-12-13 09:11:43 +0100

    Merge functions to allow firmware load cleanup.

--- drivers/bluetooth/nokia_fw.c
+++ drivers/bluetooth/nokia_fw.c
@@ -33,75 +33,71 @@
 
 #define FW_NAME_BCM2048		"bcmfw.bin"
 
-static int fw_pos;
-
 #define BT_DBG printk
 
 
 /* Read fw. Return length of the command. If no more commands in
  * fw 0 is returned. In error case return value is negative.
  */
-static int h4p_read_fw_cmd(struct h4p_info *info, const struct firmware *fw_entry)
-{
-	unsigned int cmd_len;
-	static int num = 0;
-	struct sk_buff *skb;
-
-	if (fw_pos >= fw_entry->size)
-		return 0;
-
-	if (fw_pos + 2 > fw_entry->size) {
-		dev_err(info->dev, "Corrupted firmware image 1\n");
-		return -EMSGSIZE;
-	}
-
-	cmd_len = fw_entry->data[fw_pos++];
-	cmd_len += fw_entry->data[fw_pos++] << 8;
-	if (cmd_len == 0)
-		return 0;
-
-	if (fw_pos + cmd_len > fw_entry->size) {
-		dev_err(info->dev, "Corrupted firmware image 2\n");
-		return -EMSGSIZE;
-	}
-
-	/* Note that this is timing-critical. If sending packets takes too
-	   long, initialization will fail. */
-	printk("Packet %d...", num);
-	if (num > 1) {
-		int cmd = fw_entry->data[fw_pos+1] + (fw_entry->data[fw_pos+2] << 8);
-		int len = fw_entry->data[fw_pos+3];
-		printk("cmd %x, len %d.", cmd, len);
-		skb = __hci_cmd_sync(info->hdev, cmd, len, fw_entry->data+fw_pos+4, 500);
-		if (IS_ERR(skb)) {
-			printk("...sending cmd failed %d\n", PTR_ERR(skb));
-			return -EIO;
-		}
-	}
-	num++;
-
-	fw_pos += cmd_len;
-
-	return 1;
-}
-
 int h4p_read_fw(struct h4p_info *info)
 {
+	int num = 0;
+	int fw_pos = 0;
+	struct sk_buff *skb;
 	const struct firmware *fw_entry = NULL;
-	int err;
+	int err = -ENOENT;
+	unsigned int cmd_len = 0;	
 
-	fw_pos = 0;	
 	err = request_firmware(&fw_entry, FW_NAME_BCM2048, info->dev);
-	if (err < 0 || !fw_entry)
-		goto err_clean;
+	if (err != 0 || !fw_entry)
+		return err;
 
-	printk("read firmware\n");
-	while ((err = h4p_read_fw_cmd(info, fw_entry))) {
+	while(1) {
+		int cmd, len;
+
+		fw_pos += cmd_len;		
+		
+		if (fw_pos >= fw_entry->size)
+			break;
+
+		if (fw_pos + 2 > fw_entry->size) {
+			dev_err(info->dev, "Corrupted firmware image 1\n");
+			err = -EMSGSIZE;
+			break;
+		}
+
+		cmd_len = fw_entry->data[fw_pos++];
+		cmd_len += fw_entry->data[fw_pos++] << 8;
+		if (cmd_len == 0)
+			break;
+
+		if (fw_pos + cmd_len > fw_entry->size) {
+			dev_err(info->dev, "Corrupted firmware image 2\n");
+			err = -EMSGSIZE;
+			break;
+		}
+
+		/* Skip first two packets */
+		if (++num <= 2)
+			continue;
+
+		/* Note that this is timing-critical. If sending packets takes too
+		   long, initialization will fail. */
+		printk("Packet %d...", num);
+
+		cmd = fw_entry->data[fw_pos+1];
+		cmd += fw_entry->data[fw_pos+2] << 8;
+		len = fw_entry->data[fw_pos+3];
+		printk("cmd %x, len %d.", cmd, len);
+
+		skb = __hci_cmd_sync(info->hdev, cmd, len, fw_entry->data+fw_pos+4, 500);
+		if (IS_ERR(skb)) {
+			dev_err(info->dev, "...sending cmd failed %d\n", PTR_ERR(skb));
+			err = -EIO;
+			break;
+		}
 	}
 
-	printk("done read firmware\n");
-
-err_clean:
 	release_firmware(fw_entry);
 	return err;
 }
