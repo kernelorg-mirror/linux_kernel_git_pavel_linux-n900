b6ae504ccadc74239cc59333697e799bd3ed42e2
Pavel <pavel@ucw.cz>
2015-04-02 22:10:13 +0200

    Fix thermal limits according to feedback.

--- drivers/thermal/ti-soc-thermal/omap3-thermal-data.c
+++ drivers/thermal/ti-soc-thermal/omap3-thermal-data.c
@@ -48,8 +48,8 @@ omap34xx_mpu_temp_sensor_registers = {
 static struct temp_sensor_data omap34xx_mpu_temp_sensor_data = {
 	.min_freq = 32768,
 	.max_freq = 32768,
-	.max_temp = -99000,
-	.min_temp = 99000,
+	.max_temp = 125000,
+	.min_temp = -40000,
 	.hyst_val = 5000,
 };
 
