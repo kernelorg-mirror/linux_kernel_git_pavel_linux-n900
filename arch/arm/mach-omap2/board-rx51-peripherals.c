8e7752d46a3d6dbe72b418afa4eff95eb56ee2cf
Pavel <pavel@ucw.cz>
2017-02-13 21:27:59 +0100

    arch/N900: hack: enable gpio-switch
    It is for some reason neccessary for camera to work.
    
    Probably something fun happening in userspace.

---
+++ arch/arm/mach-omap2/board-rx51-peripherals.c
@@ -0,0 +1,51 @@
+/*
+ * linux/arch/arm/mach-omap2/board-rx51-peripherals.c
+ *
+ * Copyright (C) 2008-2009 Nokia
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/gpio.h>
+#include <linux/gpio_keys.h>
+#include <linux/gpio/machine.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/timer.h>
+
+static struct platform_driver gpio_sw_driver = {
+	.driver		= {
+		.name	= "gpio-switch",
+	},
+};
+
+static int __init gpio_sw_init(void)
+{
+	int r;
+
+	printk(KERN_INFO "OMAP GPIO switch handler initializing\n");
+
+	r = platform_driver_register(&gpio_sw_driver);
+	if (r)
+		return r;
+
+	platform_device_register_simple("gpio-switch",
+							       -1, NULL, 0);
+	return 0;
+}
+
+static void __exit gpio_sw_exit(void)
+{
+}
+
+#ifndef MODULE
+late_initcall(gpio_sw_init);
+#else
+module_init(gpio_sw_init);
+#endif
+module_exit(gpio_sw_exit);
