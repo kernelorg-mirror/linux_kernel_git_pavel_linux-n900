ba31af41f37b2c8aca8b442bfb34e92bcce91d79
Pavel <pavel@ucw.cz>
2017-02-18 22:30:36 +0100

    Call the needed function directly. Main camera still works.

--- drivers/media/platform/omap3isp/ispccp2.c
+++ drivers/media/platform/omap3isp/ispccp2.c
@@ -176,15 +176,9 @@ static int ccp2_if_enable(struct isp_ccp2_device *ccp2, u8 enable)
 		/* Struct isp_bus_cfg has union inside */ 
 		buscfg = &((struct isp_bus_cfg *)sensor->host_priv)->bus.ccp2;
 
-		if (enable) {
-			printk("isp = %p\n", isp->isp_csiphy1.isp);
-			isp->isp_csiphy1.isp = isp;
-			csiphy_routing_cfg_3430(&isp->isp_csiphy1, ISP_INTERFACE_CCP2B_PHY1, true, !!buscfg->phy_layer);
-		} else {
-			csirxfe = 0;
-			regmap_write(isp->syscon, isp->syscon_offset, csirxfe);
-		}
-
+		printk("isp = %p\n", isp->isp_csiphy1.isp);
+		isp->isp_csiphy1.isp = isp;
+		csiphy_routing_cfg_3430(&isp->isp_csiphy1, ISP_INTERFACE_CCP2B_PHY1, enable, !!buscfg->phy_layer);
 	}
 
 	/* Enable/Disable all the LCx channels */
