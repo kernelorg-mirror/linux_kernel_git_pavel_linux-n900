a2a84c393001bc4a446ff13425e1c7509f3a74fb
Pavel <pavel@ucw.cz>
2017-10-26 11:11:24 +0200

    With these settings, I get
    Uncompressing Linux 4.13... done, booting the kernel.
    
    and kernel boots. Display is still empty.

--- arch/arm/boot/compressed/misc.c
+++ arch/arm/boot/compressed/misc.c
@@ -158,7 +158,7 @@ decompress_kernel(unsigned long output_start, unsigned long free_mem_ptr_p,
 
 	arch_decomp_setup();
 
-	putstr("Uncompressing Linux...");
+	putstr("Uncompressing Linux 4.13...");
 	ret = do_decompress(input_data, input_data_end - input_data,
 			    output_data, error);
 	if (ret)
