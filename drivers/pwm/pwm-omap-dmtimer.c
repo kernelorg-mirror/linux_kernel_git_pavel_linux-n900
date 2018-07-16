0a74b11440d0fa4ac3abac7d57076e81143434cd
Pavel <pavel@ucw.cz>
2018-07-16 09:12:40 +0200

    When all the drivers are built-in, timers are not ready	and probing fails. Fix that	     with -EPROBE_DEFFER.
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/pwm/pwm-omap-dmtimer.c
+++ drivers/pwm/pwm-omap-dmtimer.c
@@ -16,6 +16,8 @@
  *   PWM driver / controller, using the OMAP's dual-mode timers.
  */
 
+#define DEBUG
+
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
@@ -251,9 +253,13 @@ static int pwm_omap_dmtimer_probe(struct platform_device *pdev)
 	u32 v;
 	int ret = 0;
 
+	printk("dmtimer_probe\n");
+
 	timer = of_parse_phandle(np, "ti,timers", 0);
 	if (!timer)
 		return -ENODEV;
+
+	printk("dmtimer_probe: have timer\n");
 
 	timer_pdev = of_find_device_by_node(timer);
 	if (!timer_pdev) {
@@ -265,7 +271,7 @@ static int pwm_omap_dmtimer_probe(struct platform_device *pdev)
 	timer_pdata = dev_get_platdata(&timer_pdev->dev);
 	if (!timer_pdata) {
 		dev_err(&pdev->dev, "dmtimer pdata structure NULL\n");
-		ret = -EINVAL;
+		ret = -EPROBE_DEFER;
 		goto put;
 	}
 
@@ -294,8 +300,11 @@ static int pwm_omap_dmtimer_probe(struct platform_device *pdev)
 		goto put;
 	}
 
+	printk("dmtimer: have timer-pwm\n");
+
 	dm_timer = pdata->request_by_node(timer);
 	if (!dm_timer) {
+		printk("dmtimer: deffering -- no timer\n");		
 		ret = -EPROBE_DEFER;
 		goto put;
 	}
@@ -305,6 +314,7 @@ put:
 	if (ret < 0)
 		return ret;
 
+	printk("dmtimer: allocating\n");
 	omap = devm_kzalloc(&pdev->dev, sizeof(*omap), GFP_KERNEL);
 	if (!omap) {
 		pdata->free(dm_timer);
@@ -338,6 +348,8 @@ put:
 
 	mutex_init(&omap->mutex);
 
+	printk("dmtimer: registering pwm\n");
+
 	ret = pwmchip_add(&omap->chip);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "failed to register PWM\n");
@@ -346,6 +358,7 @@ put:
 	}
 
 	platform_set_drvdata(pdev, omap);
+	printk("dmtimer: all good\n");
 
 	return 0;
 }
