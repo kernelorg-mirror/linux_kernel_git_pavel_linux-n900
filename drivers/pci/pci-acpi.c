1381386c23b918512906e0bff50445cc02844bbd
Pavel <pavel@ucw.cz>
2018-10-28 12:54:14 +0100

    recheck: PCIe aspm tweak. Should decrease power consumption on x60?

--- drivers/pci/pci-acpi.c
+++ drivers/pci/pci-acpi.c
@@ -897,7 +897,7 @@ static int __init acpi_pci_init(void)
 
 	if (acpi_gbl_FADT.boot_flags & ACPI_FADT_NO_ASPM) {
 		pr_info("ACPI FADT declares the system doesn't support PCIe ASPM, so disable it\n");
-		pcie_no_aspm();
+//		pcie_no_aspm();
 	}
 
 	ret = register_acpi_bus_type(&acpi_pci_bus);
